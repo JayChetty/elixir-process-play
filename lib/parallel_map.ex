0eee282acff426d4105f6190e42693f33e4d7cd9
JayChetty <jaypaulchetty@gmail.com>
2016-07-12 10:18:29 +0100

    can see parallel speed ups!

--- lib/parallel_map.ex
+++ lib/parallel_map.ex
@@ -1,3 +1,10 @@
+defmodule Mocks do
+  def expensive(number) do
+    Enum.reduce(1..100000, fn(element, reducer)-> reducer + :math.sqrt(element) * number end)
+  end
+end
+#
+
 defmodule ParallelMap do
   def p_map(list, fun) do
     # me = self
@@ -29,3 +36,18 @@ defmodule MapWorker do
     end
   end
 end
+
+
+
+list = Enum.to_list 1..10
+
+{time, result} = :timer.tc( ParallelMap, :p_map, [list, &Mocks.expensive/1 ])
+IO.puts inspect time
+sum = Enum.reduce(result, fn(element, reducer)-> reducer + element end)
+IO.puts inspect sum
+
+
+{time, result} = :timer.tc( Enum, :map, [list, &Mocks.expensive/1 ])
+IO.puts inspect time
+sum = Enum.reduce(result, fn(element, reducer)-> reducer + element end)
+IO.puts inspect sum
--- test/map_speed_test.exs
+++ test/map_speed_test.exs
@@ -1,18 +1,22 @@
 defmodule Mocks do
   def expensive(number) do
-    Enum.reduce(1..100, fn(element, reducer)-> reducer + :math.sqrt(element) * number end)
+    Enum.reduce(1..100000, fn(element, reducer)-> reducer + :math.sqrt(element) * number end)
   end
 end
+#
+# defmodule MapSpeedTest do
+#   use ExUnit.Case
+#
+#   test "p map" do
+#     list = Enum.to_list 1..100
+#     # test_func = fn(a) -> a * 2 end
+#     # mapped = ProcessPlay.my_map(list, &Mocks.expensive/1)
+#     mapped = ParallelMap.p_map(list, &Mocks.expensive/1)
+#     # mapped = Enum.map(list, &Mocks.expensive/1)
+#     IO.puts( inspect Enum.reduce(mapped, fn(element, reducer)-> reducer + element end) )
+#   end
+# end
 
-defmodule MapSpeedTest do
-  use ExUnit.Case
 
-  test "p map" do
-    list = Enum.to_list 1..1000000
-    # test_func = fn(a) -> a * 2 end
-    # mapped = ProcessPlay.my_map(list, &Mocks.expensive/1)
-    mapped = ParallelMap.p_map(list, &Mocks.expensive/1)
-    # mapped = Enum.map(list, &Mocks.expensive/1)
-    IO.puts( inspect Enum.reduce(mapped, fn(element, reducer)-> reducer + element end) )
-  end
-end
+list = Enum.to_list 1..100
+{time, result} = :timer.tc( ProcessPlay, :my_map, [list, &Mocks.expensive/1 ])
